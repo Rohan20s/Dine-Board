9a342f520855c74ebf2500b0dc34fa51e1fccff0
Aaryan <aaryandewangan95759@gmail.com>
2025-07-22 22:43:42 +0530

    some styles are changed in login page  ,  tailwind @import type changed to @use in global scss file

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -40,7 +40,6 @@ const LoginPage = () => {
 
   return (
     <div className="bg-gray-100 min-h-screen">
-      <div className="mx-auto px-4">
         <Formik
           initialValues={initialValues}
           validationSchema={validationSchema}
@@ -48,18 +47,18 @@ const LoginPage = () => {
         >
           {(formik) => (
             <>
-              <div className="py-2">
+              <div className="py-2 flex justify-center">
                 <a href="#"><img src="/assets/images/logo-dark.svg" alt="img" /></a>
               </div>
               <Form>
-                <div className="max-w-lg mx-auto my-5 w-full bg-white rounded-lg shadow-md">
+                <div className="max-w-md mx-auto  w-full my-5 bg-white rounded shadow-md">
                   <div className="p-8">
                     <div className="flex justify-between items-end mb-4">
                       <h3 className="mb-0 text-2xl font-bold">{loginTexts.title}</h3>
                       <a
                         href="#"
                         onClick={handleLoginRedirect}
-                        className="text-blue-600 hover:underline text-sm"
+                        className="text-blue-600 hover:underline text-sm text-decoration-none"
                       >
                         {loginTexts.notHaveAccount}
                       </a>
@@ -71,8 +70,7 @@ const LoginPage = () => {
                         id="email"
                         name="email"
                         placeholder="Email Address"
-                        className={`block w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 ${formik.touched.email && formik.errors.email ? 'border-red-500' : 'border-gray-300'
-                          }`}
+                        className={`block w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 hover:bg-gray-100 ${formik.touched.email && formik.errors.email ? 'border-red-500' : 'border-gray-300'}`}
                       />
                       <ErrorMessage name="email" component="div" className="text-red-500 text-xs mt-1" />
                     </div>
@@ -84,35 +82,33 @@ const LoginPage = () => {
                         id="password"
                         name="password"
                         placeholder="Password"
-                        className={`block w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 ${formik.touched.password && formik.errors.password ? 'border-red-500' : 'border-gray-300'
-                          }`}
+                        className={`block w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 hover:bg-gray-100 ${formik.touched.password && formik.errors.password ? 'border-red-500' : 'border-gray-300'}`}
                       />
                       <ErrorMessage name="password" component="div" className="text-red-500 text-xs mt-1" />
                     </div>
                     <div className="flex mt-1 justify-end">
-                      <a
-                        href="#"
+                      <p
+
                         onClick={handleForgetRedirect}
-                        className="text-blue-600 hover:underline text-sm font-medium"
-                      >
+                        className="text-gray-600 hover:underline text-lg font-medium text-decoration-none cursor-pointer mb-0" >
                         {loginTexts.forgotPassword}
-                      </a>
+                      </p>
                     </div>
                     <div className="mt-6">
                       <button
                         type="button"
-                        className="w-full py-2 px-4 bg-blue-600 text-white rounded hover:bg-blue-700 font-semibold transition"
+                        className="w-full py-2 px-4 bg-blue-600 text-white rounded hover:bg-blue-800 font-semibold transition"
                       >
                         {loginTexts.login}
                       </button>
                     </div>
-                    <div className="mt-4 text-center">
+                    <div className="my-3 text-center">
                       <span className="text-gray-500">{loginTexts.loginWith}</span>
                     </div>
-                    <div className="mt-2">
+                    <div>
                       <button
                         type="button"
-                        className="w-full flex items-center justify-center gap-2 py-2 px-4 bg-gray-100 text-gray-700 rounded hover:bg-gray-200 border border-gray-300 mt-2"
+                        className="w-full flex items-center justify-center gap-2 py-2 px-4 bg-gray-100 text-gray-700 rounded hover:bg-gray-200 border border-gray-300"
                       >
                         <img src="/assets/images/authentication/google.svg" alt="img" className="h-5 w-5" />
                         <span className="hidden sm:inline-block">{loginTexts.google}</span>
@@ -124,7 +120,6 @@ const LoginPage = () => {
             </>
           )}
         </Formik>
-      </div>
     </div>
   );
 }
